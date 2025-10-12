7f21f8e56d6f64c2088854f7e17c01e0eb286787
Santhosh J R <137148886+Santhosh7jr@users.noreply.github.com>
2025-10-12 19:13:46 +0530

    Delete Django directory

--- Django/Django_Projects/DataEncryption/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import homepage, encrypt, decrypt
-
-urlpatterns = [
-    path("", homepage, name="home"),
-    path("encrypt/", encrypt, name="encrypt"),
-    path("decrypt/", decrypt, name="decrypt"),
-]
