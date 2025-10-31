3b7d709a902e746e7efa42cb31fe9de2ebf3d896
Santhosh J R <137148886+Santhosh7jr@users.noreply.github.com>
2025-10-31 18:00:37 +0530

    Add files via upload

---
+++ Eloquent_js/Values, Types, and Operators/main.js
@@ -0,0 +1,45 @@
+//getting the datatype of the value
+/*console.log(typeof 3);
+
+let type = typeof 'x';
+console.log(type) */
+
+
+//binary operators operation
+/*console.log(-(2 - 3));
+console.log(5 + 5);
+console.log(2 * 4);
+console.log( 4 / 4);*/
+
+
+//boolean operations
+/*console.log(3 < 2);
+console.log(3 > 2);
+console.log('A' < 'a');
+console.log("Hello" < "hello"); // decides based on the first letter. here, capital letter is lesser than the small letter.
+console.log("hello" < "Hello");
+console.log(NaN == NaN); // the only value in js which is not equal to itself.*/
+
+
+//logical operators
+/*console.log(true && true);
+console.log(true && false);
+console.log(true || true);
+console.log(false || false);
+console.log(! true);
+console.log(!! true);
+console.log(true ? 1 : 2);*/
+
+
+//Empty values
+/*console.log(undefined);
+console.log(null);*/
+
+
+//Automatic Type Conversion
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log(false == 0);
+console.log(true == 1);
+console.log(null == undefined);
