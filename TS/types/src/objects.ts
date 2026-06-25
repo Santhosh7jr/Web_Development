712b24f081c4945c18063dbe57324fe7a4639545
Santhosh J R <jrsanthosh89@gmail.com>
2026-06-25 12:26:35 +0530

    types with Objects.

---
+++ TS/types/src/objects.ts
@@ -0,0 +1,21 @@
+type Student = {
+  name : string;
+  age : number;
+  marks : number;
+}
+
+const student1 : Student = {
+  name : "guru",
+  age : 21,
+  marks : 96
+}
+
+const student2 : Student = {
+  name : "giri",
+  age : 21,
+  marks : 99,
+  topper : true
+}
+
+console.log(student1);
+console.log(student2);
