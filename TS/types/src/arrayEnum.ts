c9359405cd7e169c324434831aaf732c31cf0c83
Santhosh J R <jrsanthosh89@gmail.com>
2026-06-29 12:15:54 +0530

    ts arrays and enums.

---
+++ TS/types/src/arrayEnum.ts
@@ -0,0 +1,25 @@
+const fruits: string[] = ["apple", "grape"];
+fruits.push("orange");
+// console.log(fruits);
+
+type Info = {
+  name : string,
+  age : number
+}
+
+const students : Info[] = [
+  {name : "guru", age : 20}
+]
+students.push({name : "giri", age : 21});
+// console.log(students);
+
+const employees : readonly Info[] = [
+  {name : "guru", age : 20}
+]
+employees.push({name : "giri", age : 21});
+// console.log(employees);
+
+const details : [string, number : 20 | 21, boolean?] = ["guru", 20, false];
+const details1 : [string, number : 20 | 21, boolean?] = ["giri", 22];
+console.log(details);
+console.log(details1);
